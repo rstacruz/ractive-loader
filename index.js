9da1a6503b020b0a43ad1ea7b6df8364acc1569d
Jonathan Dumaine <jonathan.dumaine@gmail.com>
2016-06-23 04:06:43 -0700

    Use resolve() to find which ractive.js to require

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 module.exports = function (source) {
-	var Ractive = require("ractive");
 
-	this.cacheable();
-	return "module.exports=" + JSON.stringify(Ractive.parse(source)) + ";";
+    var callback = this.async();
+
+    this.cacheable();
+
+    this.resolve(this.context, "ractive", function(err, result) {
+
+        if(err) return callback(err);
+
+        var Ractive = require(result);
+        Ractive.DEBUG = false;
+        
+        callback(null, "module.exports=" + JSON.stringify(Ractive.parse(source)) + ";");
+    });
+
 };
